7b7231ff83cd667d80cba0252e014e147f584e15
shrimpcod <prog2708@gmail.com>
2026-01-26 20:49:04 +0500

    feat: добавил is_active, is_superuser fields to User Model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Enum as SQLEnum
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime, Enum as SQLEnum
 from sqlalchemy.orm import relationship
-from .base import Base 
+from .base import Base
 import enum
+from datetime import datetime
 
 class UserType(str, enum.Enum):
     PRIVATE = "private"
@@ -17,7 +18,11 @@ class User(Base):
     fullname = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     user_type = Column(SQLEnum(UserType), default=UserType.PRIVATE, nullable=False)
-    ip_address = Column(String, nullable=False)
+    ip_address = Column(String, nullable=True)
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
 
     agency_id = Column(Integer, ForeignKey("agencies.id"), nullable=True)
     position_id = Column(Integer, ForeignKey("positions.id"), nullable=True)
