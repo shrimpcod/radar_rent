4f5a3704429a4c26840b29791a38a224b7192333
shrimpcod <prog2708@gmail.com>
2026-01-26 23:17:05 +0500

    refactor: исправил inclide to include

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -2,4 +2,4 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import auth
 
 api_router = APIRouter()
-api_router.inclide_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
