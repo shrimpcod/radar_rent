f30f12fc22f7ee743f2238022ef5b9d9acce05a6
shrimpcod <prog2708@gmail.com>
2026-02-16 21:39:38 +0500

    feat: добавил пути в главный роутер

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth
+from app.api.v1.endpoints import auth, agencies, positions, teams, team_members, users
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(agencies.router, prefix="/agencies", tags=["agencies"])
+api_router.include_router(positions.router, prefix="/positions", tags=["positions"])
+api_router.include_router(teams.router, prefix="/teams", tags=["teams"])
+api_router.include_router(team_members.router, prefix="/team_members", tags=["team-members"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
