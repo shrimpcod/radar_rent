5d6d8ce52ce417796b13546f2bd5793af6bd2dad
shrimpcod <prog2708@gmail.com>
2026-03-05 21:09:45 +0500

    реализовал взаимодействие с ботом с помощью планировщика и настроил отправку уведомлений

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,13 +2,42 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1 import api_router
 from app.core.config import settings
+from app.db.session import get_db
+from app.services.cian_parser_service import CianParserService
+
+from contextlib import asynccontextmanager
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
+scheduler = AsyncIOScheduler()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler.start()
+    
+    async def parse_cian_taks():
+        async for db in get_db():
+            service = CianParserService(db)
+            await service.fetch_and_save_leads()
+            break
+    
+    scheduler.add_job(
+        parse_cian_taks,
+        trigger=IntervalTrigger(minutes=5, jitter=60),
+        id='cian_parser',
+        name='Parser cian',
+        replace_existing=True
+    )
+
+    await parse_cian_taks()
+    yield
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     description=settings.DESCRIPTION,
     version="0.1.0",
     docs_url="/api/docs",
-    redoc_url="/api/redoc"
+    redoc_url="/api/redoc",
+    lifespan=lifespan
 )
 
 app.add_middleware(
