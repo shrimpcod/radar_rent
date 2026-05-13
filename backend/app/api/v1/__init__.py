adad76dcff8e93a85398dfa9eccbb1e8758e711a
shrimpcod <prog2708@gmail.com>
2026-05-13 12:05:42 +0500

    Добавил новый эндпоинт для таблицы действий. Добавил этот эндпоинт в глобальную маршрутизацию файла init

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, agencies, positions, teams, team_members, users, leads, ws
+from app.api.v1.endpoints import auth, agencies, positions, teams, team_members, users, leads, ws, lead_actions
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
@@ -10,3 +10,4 @@ api_router.include_router(team_members.router, prefix="/team_members", tags=["te
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(leads.router, prefix="/leads", tags=["leads"])
 api_router.include_router(ws.router, tags=["websocket"])
+api_router.include_router(lead_actions.router, prefix="/lead_actions", tags=["lead-actions"])
